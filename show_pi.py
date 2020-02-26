384ec5b221e426d039c1bcc4a6e2cc54f827e75e
Martin Dreher <martin@thinkpad>
2020-02-26 11:25:13 +0100

    merged conflict

--- show_pi.py
+++ show_pi.py
@@ -4,7 +4,11 @@ def approximate_pi():
     return 355 / 113
 
 def show_pi():
+<<<<<<< HEAD
     print("π is {:.5f}".format(approximate_pi()))
+=======
+    print("pi is approximately {:.8f}".format(approximate_pi()))
+>>>>>>> d649848... show more pi digits
 
 if __name__ == "__main__":
     show_pi()
