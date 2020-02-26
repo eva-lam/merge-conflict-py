06bf6dbb3fb97c085511d9465eafb2a92bbfb5e1
Martin Dreher <martin@thinkpad>
2020-02-26 11:23:48 +0100

    cherry-picked

--- show_pi.py
+++ show_pi.py
@@ -5,10 +5,14 @@ def approximate_pi():
 
 def show_pi():
 <<<<<<< HEAD
+<<<<<<< HEAD
     print("π is {:.5f}".format(approximate_pi()))
 =======
     print("pi is approximately {:.8f}".format(approximate_pi()))
 >>>>>>> d649848... show more pi digits
+=======
+    print("π is {:.5f}".format(approximate_pi()))
+>>>>>>> 9c946a7... make pi symbol unicode
 
 if __name__ == "__main__":
     show_pi()
